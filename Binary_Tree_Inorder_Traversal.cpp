c75f7bd4a729724851368792c6ee83ad13b562f6
piyush0074 <piyushmakwana74@gmail.com>
2021-03-22 13:07:22 +0530

    Leetcode tag- Tree, #94 Solution

---
+++ Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/binary-tree-inorder-traversal/
+*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL) return ans;
+        TreeNode *curr = root;
+        stack<TreeNode *> s;
+        while(curr != NULL || !s.empty()){
+            while(curr != NULL){
+                s.push(curr);
+                curr = curr->left;
+            }
+            curr = s.top();
+            ans.push_back(curr->val);
+            s.pop();
+            curr = curr->right;
+        }
+        return ans;
+    }
+};
